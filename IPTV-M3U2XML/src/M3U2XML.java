ad1097209cbeca1ae8d0facb7e92b39030ac3ad9
duran <duran@DESKTOP-IHDNLF1>
2017-01-04 09:41:14 +0100

    finished and working, minor updates will come.

---
+++ IPTV-M3U2XML/src/M3U2XML.java
@@ -0,0 +1,313 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+
+import javax.swing.JFileChooser;
+
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Text;
+
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+
+import com.sun.org.apache.xml.internal.serialize.XMLSerializer;
+import com.sun.org.apache.xml.internal.serialize.OutputFormat;
+
+
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.apache.commons.io.FilenameUtils;
+import java.awt.Font;
+
+
+import javax.swing.JTextArea;
+
+
+public class M3U2XML {
+	
+	String str;
+	int counter = 0;
+	int counter_channel = 0;
+	int counter_address = 0;
+	
+	ArrayList<String> channel = new ArrayList<String>();
+	ArrayList<String> address = new ArrayList<String>();
+	
+	String[] channelArray = new String[channel.size()];
+	String[] addressArray = new String[address.size()];
+	
+
+	File nameDir, namePath;
+	
+	
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					M3U2XML window = new M3U2XML();
+					window.frame.setVisible(true);
+				
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public M3U2XML() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 429, 375);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setBounds(15, 16, 377, 195);
+		textArea.setWrapStyleWord(true);
+		textArea.setLineWrap(true);
+		textArea.setEditable(false);
+		Font font = textArea.getFont();
+		float size = font.getSize() + 5.0f;
+		textArea.setFont( font.deriveFont(size) );
+		frame.getContentPane().add(textArea);
+		
+		JButton btnNewButton = new JButton("Open m3u file");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				
+//				
+
+				
+				JFileChooser chooser = new JFileChooser();
+				File f = new File("C:/");
+				
+				int checker;
+				chooser.setCurrentDirectory(f);
+				chooser.setDialogTitle("Open m3u file");
+				checker = chooser.showOpenDialog(null);
+				
+				if (checker == JFileChooser.APPROVE_OPTION) {
+//					nameDir = chooser.getCurrentDirectory();
+					namePath = chooser.getSelectedFile();
+//					System.out.println("The name of directory: " + nameDir.getName());
+//					System.out.println("The name of path: " + namePath.getAbsolutePath());
+					
+					//Read file
+					try {
+						
+						FileReader fr = new FileReader(namePath);
+						BufferedReader br = new BufferedReader(fr);
+						
+
+						while ((str = br.readLine()) != null) {
+							
+							
+							if (str.contains("#EXTM3U")) {
+								continue;
+							}
+							
+				
+							else if (counter % 2 == 0) {
+								
+								channel.add(str);
+								
+								counter_channel++;
+							}
+							else {
+								
+								address.add(str);
+								counter_address++;
+							}
+							counter++;
+						}
+						
+						channelArray = channel.toArray(channelArray);
+						addressArray = address.toArray(addressArray);
+						
+						for (int i = 0; i < channelArray.length; i++ ) {
+							
+
+							channelArray[i] = channelArray[i].replace("#EXTINF:-1,", "");
+							
+						}
+						
+						
+						
+						for (int i = 0; i < addressArray.length; i++ ) {
+							
+							
+							addressArray[i] = "plugin://plugin.video.f4mTester/?url="+address.get(i)+"&amp;streamtype=TSDOWNLOADER";
+							
+						}
+						
+						
+						
+						System.out.println(Arrays.toString(channelArray));
+						System.out.println(Arrays.toString(addressArray));
+				
+						
+						
+						System.out.println(counter + " total lines");
+						System.out.println(counter_channel + " total even lines");
+						System.out.println(counter_address + " total odd lines");
+						
+						System.out.println("Channel array total size " + channelArray.length);
+						System.out.println("Address array total size " + addressArray.length);
+						
+						br.close();
+				
+						textArea.setText("You have imported " + namePath + "\n" +  "Please click \"Create XML File\"");
+						
+					} catch (IOException e) {
+						System.out.println("File not found!");
+						e.printStackTrace();
+					}
+					
+					
+				}
+				else {
+					
+					JOptionPane.showMessageDialog(null, "You have clicked cancel", "Cancel dialog box", JOptionPane.WARNING_MESSAGE);
+				}
+				
+				
+				
+			}
+		});
+		btnNewButton.setBounds(15, 249, 140, 50);
+		frame.getContentPane().add(btnNewButton);
+		
+		JButton btnCreateXmlFile = new JButton("Create XML file");
+		btnCreateXmlFile.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0){
+				
+				//DocumentBuilderFactory
+				DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+				//DocumentBuilder
+				DocumentBuilder docBuilder = null;
+				try {
+					docBuilder = docFactory.newDocumentBuilder();
+				} catch (ParserConfigurationException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				//Document
+				Document xmlDoc = docBuilder.newDocument();
+				
+				
+				//Build XML Elements and Text Nodes
+				//<streamingInfos>
+				//	<item>
+				//		<title>title or channel name</title>
+				//		<link>link address</link>
+				//	</item>
+				//</streamingInfos>
+				
+				
+				int size = channel.size();
+				
+				Element rootElement = xmlDoc.createElement("streamingInfos");
+				
+				for (int i = 0; i < size ; i++) {
+				
+				Element mainElement = xmlDoc.createElement("item");
+//				mainElement.setAttribute("sku", "123456");
+				
+				
+				
+				Element title = xmlDoc.createElement("title");
+				Text titleText = xmlDoc.createTextNode(channelArray[i]);
+				title.appendChild(titleText);
+				
+				
+				Element link = xmlDoc.createElement("link");
+				Text linkText = xmlDoc.createTextNode(addressArray[i]);
+				link.appendChild(linkText);
+				
+				mainElement.appendChild(title);
+				mainElement.appendChild(link);
+				
+				
+				rootElement.appendChild(mainElement);
+				}
+				
+				xmlDoc.appendChild(rootElement);
+				
+				
+				
+				//Set OutputFormat
+				OutputFormat outFormat = new OutputFormat(xmlDoc);
+				outFormat.setIndenting(true);
+				
+				//Declare the File
+				String fileNameWithOutExt = FilenameUtils.removeExtension(namePath.getName());
+				String newFile = fileNameWithOutExt + ".xml"; 
+				File xmlFile = new File(newFile);
+				
+				//Declare the FileOutputStream
+				FileOutputStream outStream = null;
+				try {
+					outStream = new FileOutputStream(xmlFile);
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				//XMLSerializer to serialize the XML data with 
+				XMLSerializer serializer = new XMLSerializer(outStream, outFormat);
+			
+				//the specific OutputFormat
+				try {
+					serializer.serialize(xmlDoc);
+					textArea.setText("You created the file "  + newFile + "!");
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			
+				
+			
+				
+			}
+		});
+		btnCreateXmlFile.setBounds(252, 249, 140, 50);
+		frame.getContentPane().add(btnCreateXmlFile);
+		
+
+	}
+}
